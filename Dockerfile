cd3a1651e8bacbec64168ce70cd0285282e0576a
Dhanika Novlisariyanti <13521132@std.stei.itb.ac.id>
2023-09-25 22:37:29 +0700

    set up docker php, gitignore, css

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,4 @@
 FROM php:8.0-apache
+WORKDIR /var/www/html
+COPY src/public/index.php .
+EXPOSE 80
---
+++ src/public/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <meta charset="UTF-8">
+        <!---Title--->
+        <title>Notflix</title>
+        <!---Icon--->
+        <link rel="icon" href="images/icon/logo.ico">
+        <link rel="stylesheet" type="text/css" href="styles/template/globals.css">
+    </head>
+    <body>
+        <h1 style="font-size: heading1;">Header 1</h1>
+        <h2 style="font-size: heading2;">Header 2</h2>
+        <h3 style="font-size: heading3;">Header 3</h3>
+        <h4 style="font-size: heading4;">Header 4</h4>
+        <p>Semoga Selesai</p>
+        <div style="display:flex; flex-direction: column; width:250px;">
+            <button class="button-red button-text red-glow">Button Red</button>
+            <button class="button-white button-text">Button White</button>
+            <button class="button-blue button-text">Button Blue</button>
+            <button class="button-green button-text">Button Green</button>
+        </div>
+    </body>
+</html>
