ffcc117175e1694be31739a6ea15f09b5414f47b
Rezapu Obed <obed.ubaidillah@gmail.com>
2023-10-01 19:06:26 +0700

    refactor: separated controller init with body

--- src/app/component/user/WatchListPage.php
+++ src/app/component/user/WatchListPage.php
@@ -17,18 +17,15 @@
 
 <body>
     <?php include(dirname(__DIR__) . "/template/NavbarUser.php");?>
+    <?php 
+        require_once DIRECTORY . '/../controller/watchlist/WatchListPageController.php';
+        $watchListPageController = new WatchListPageController();
+        $watchListPageController->setUserID(1);
+    ?>
     <h1>Watch List</h1>
     <section class='cards'>
-        <!-- <div class='card'>
-            <img src="storage/2f426e6052e1809ef93fd60ac22b03fd.jpg" alt="Midsommar">
-        </div> -->
         <?php 
-            require_once DIRECTORY . '/../controller/watchlist/WatchListPageController.php';
-            $watchListPageController = new WatchListPageController();
-            $watchListPageController->setUserID(1);
-
-            // print_r($watchListPageController->getWatchListFilms());
-            echo $watchListPageController->generateCards();
+            $watchListPageController->generateCards();
         ?>
     </section>
 
