0203053438d40134b06addfba63809be4539ab63
Rezapu Obed <obed.ubaidillah@gmail.com>
2023-10-01 18:42:02 +0700

    feat: Add function for getting films

--- src/app/models/watchlist.php
+++ src/app/models/watchlist.php
@@ -13,11 +13,14 @@ class WatchListModel{
     /**Get All Film in user watchlist butuh -> USER ID */
     public function getWatchList($userID){
         $this->db->callQuery('SELECT film_id FROM ' . $this->table . ' WHERE user_id = ' . $userID);
-        return $this->db->fetchResult();
+        return $this->db->fetchAllResult();
+    }
+    public function getWatchListFilms($userID){
+        $this->db->callQuery('SELECT * FROM ' . $this->table . ' NATURAL INNER JOIN film WHERE user_id = ' . $userID);
+        return $this->db->fetchAllResult();
     }
     /**Add new film to watchlist */
     public function addFilmToWatchList($userID){
-
     }
     /**Delete/remove film in watchlist */
     public function deleteFilmFromWatchList($userID){
