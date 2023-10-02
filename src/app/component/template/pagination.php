16bc244000aa34032f62f24a5a6d06e1ae8e94f8
Rezapu Obed <obed.ubaidillah@gmail.com>
2023-10-02 14:11:29 +0700

    feat(pagination): add pagination template

---
+++ src/app/component/template/pagination.php
@@ -0,0 +1,31 @@
+<!--
+    @total_records
+    @items_per_page
+    @current_page
+-->
+<div class='pagination'>
+    <?php
+        if($total_records) $total_records=$total_records['count'];
+        $limitpage = 4;
+
+        $totalpages = ceil($total_records/$items_per_page);
+        
+        $start = ($current_page===1) ? 1 : $current_page-1;
+        $bound = $start+$limitpage-1;
+        if($bound > $totalpages){
+            $start = $totalpages-$limitpage+1;
+            $bound = $totalpages;
+        }
+        if($current_page!=1){
+            $target = '<';
+            include (DIRECTORY . "/../component/template/paginationButton.php");
+        }
+        for($i=$start; $i<=$bound; $i++){
+            $target = $i;
+            include(DIRECTORY . "/../component/template/paginationButton.php");
+        }
+        if($current_page!=$totalpages){
+            $target = '>';
+            include (DIRECTORY . "/../component/template/paginationButton.php");
+        }?>
+</div>
