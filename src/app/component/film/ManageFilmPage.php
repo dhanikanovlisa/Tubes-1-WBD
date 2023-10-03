02cb56f9cd026ddafeec6a2446611407556d6dba
Dhanika Novlisariyanti <13521132@std.stei.itb.ac.id>
2023-10-03 15:45:12 +0700

    add navbar to page

--- src/app/component/film/ManageFilmPage.php
+++ src/app/component/film/ManageFilmPage.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" type="text/css" href="styles/template/Navbar.css">
     <link rel="stylesheet" type="text/css" href="styles/template/cardMovie.css">
     <!---Page specify CSS--->
+    <script type="text/javascript" src="javascript/navbar/navbar.js" defer></script>
     <link rel="stylesheet" type="text/css" href="styles/film/manageFilm.css">
 </head>
 
