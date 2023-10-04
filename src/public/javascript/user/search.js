28c42dc2fe779d70e6c9d84addfc3822b28802c8
Rezapu Obed <obed.ubaidillah@gmail.com>
2023-10-04 15:06:41 +0700

    fix(search): param doesnt read

--- src/public/javascript/user/search.js
+++ src/public/javascript/user/search.js
@@ -1,14 +1,16 @@
 const form = document.forms['search-film'];
-console.log(form);
 
 const search = form.elements['title'];
 const orderby = form.elements['orderby'];
 const genre = form.elements['genre'];
+
 const resultContainer = document.getElementById('result-container');
+const paginationContainer = document.getElementById('pagination-container');
 
 const fetchResults = (search_title=null, search_orderby=null, search_genre=null)=>{
-    const xhr = new XMLHttpRequest();
-    const params = new URLSearchParams();
+    const xhr_film = new XMLHttpRequest();
+    const xhr_pagination = new XMLHttpRequest();
+    const params = new URLSearchParams(window.location.search);
 
     if(search_title){
         params.set('title', search_title);
@@ -20,17 +22,19 @@ const fetchResults = (search_title=null, search_orderby=null, search_genre=null)
         params.set('genre',search_genre);
     }
     const location = '/search/search?'+params.toString();
-    xhr.open('GET', location, true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.send();
+    xhr_film.open('GET', location, true);
+    xhr_film.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr_film.send();
 
-    xhr.onload = (ev)=>{
-        if(xhr.readyState !== XMLHttpRequest.DONE) return;
-        if(xhr.status==200){
-            resultContainer.innerHTML = xhr.responseText;
+    xhr_film.onload = (ev)=>{
+        if(xhr_film.readyState !== XMLHttpRequest.DONE) return;
+        if(xhr_film.status==200){
+            resultContainer.innerHTML = xhr_film.responseText;
             // window.history.replaceState(null, document.title, '/search?'+params.toString());
         }
     }
+
+    //TODO: handle pagination
 }
 
 search.addEventListener('input', async (ev)=>{
