147b0b53a1bc9ab24c5ddca7006487dfabc5eb9b
Dhanika Novlisariyanti <13521132@std.stei.itb.ac.id>
2023-10-05 14:06:20 +0700

    edit confirmation modal message

--- src/app/component/film/DetailFilmPage.php
+++ src/app/component/film/DetailFilmPage.php
@@ -82,7 +82,7 @@
                                 <div class="modal-content red-glow">
                                     <div class="whole">
                                         <div class="title-modal-container">
-                                            <h3 class="text-black" id="main-message">Are you sure you want to Delete This?</h3>
+                                            <h3 class="text-black" id="main-message">Are you sure you want to delete <?php echo $filmData['title']?>?</h3>
                                             <p class="text-black" id="description-message">This will be gone</p>
                                         </div>
                                         <div class="button-modal-container">
