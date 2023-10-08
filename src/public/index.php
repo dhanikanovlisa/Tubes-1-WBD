c8d9855c31675f5ec35fc409785debdcb8ce850c
althaafka <althaafka004@gmail.com>
2023-10-08 12:08:41 +0700

    add database

--- src/public/index.php
+++ src/public/index.php
@@ -5,7 +5,6 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-
 $router = new parserrouting($route);
 $uri = $_SERVER["REQUEST_URI"];
 $method = $_SERVER["REQUEST_METHOD"];
