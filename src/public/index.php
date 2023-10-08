91028c1a809a1efa3c1423145a80c1ce6162b940
althaafka <althaafka004@gmail.com>
2023-10-08 11:55:35 +0700

    fix session

--- src/public/index.php
+++ src/public/index.php
@@ -3,18 +3,8 @@
 require_once "../app/init.php";
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
-
-    $current_time = time();
-    $_SESSION['created_at'] = $current_time;
 }
 
-if (session_status() === PHP_SESSION_ACTIVE) {
-    $current_time = time();
-    if (isset($_SESSION['created_at']) && ($current_time - $_SESSION['created_at'] > 24*60*60)) {
-        session_unset();
-        session_destroy();
-    }
-}
 
 $router = new parserrouting($route);
 $uri = $_SERVER["REQUEST_URI"];
